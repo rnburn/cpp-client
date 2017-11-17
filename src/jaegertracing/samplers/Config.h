273998082fe9d502746ffd3b0d843d0eb2b62a9f
Ryan Burn <ryan.burn@gmail.com>
2017-11-16 21:17:38 -0800

    Statically link in standard c/c++ libs.

--- src/jaegertracing/samplers/Config.h
+++ src/jaegertracing/samplers/Config.h
@@ -60,9 +60,8 @@ class Config {
 
         const auto type =
             utils::yaml::findOrDefault<std::string>(configYAML, "type", "");
-        const double param = 1.0;
-        /* const auto param = */
-        /*     utils::yaml::findOrDefault<double>(configYAML, "param", 0); */
+        const auto param =
+            utils::yaml::findOrDefault<double>(configYAML, "param", 0);
         const auto samplingServerURL = utils::yaml::findOrDefault<std::string>(
             configYAML, "samplingServerURL", "");
         const auto maxOperations =
