5ea538701c3640fa22750bad93460660db7c07f2
Ryan Burn <ryan.burn@gmail.com>
2017-11-17 13:30:34 -0800

    Add snippet to demonstrate loading problem.

--- src/jaegertracing/DynamicLoad.cpp
+++ src/jaegertracing/DynamicLoad.cpp
@@ -1,11 +1,19 @@
 #include "jaegertracing/DynamicLoad.h"
 
 #include "jaegertracing/Tracer.h"
+#include <iostream>
+#include <sstream>
 
 int make_libjaegertracing_tracer(const char* config,
                                  void* tracerSharedPtrPtr,
                                  void* errorMsgStringPtr)
 {
+    // When running on Docker-ubuntu the integers are ignored by iostreams.
+    std::istringstream iss{"123"};
+    int x = 0;
+    iss >> x;
+    std::cout << "x = " << x << std::endl;
+
     auto& tracer = *static_cast<std::shared_ptr<opentracing::Tracer>*>(
         tracerSharedPtrPtr);
     auto& errorMsg = *static_cast<std::string*>(errorMsgStringPtr);
