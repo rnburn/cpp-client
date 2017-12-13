db03b303b0c3cf3c95c13af3c6b1ee287a584933
Ryan Burn <ryan.burn@gmail.com>
2017-12-12 16:18:04 -0800

    Implement TracerFactory.

--- src/jaegertracing/TracerFactory.cpp
+++ src/jaegertracing/TracerFactory.cpp
@@ -1,11 +1,42 @@
 #include "TracerFactory.h"
 
+#include "jaegertracing/Tracer.h"
+
 namespace jaegertracing {
 
 opentracing::expected<std::shared_ptr<opentracing::Tracer>>
 TracerFactory::MakeTracer(const char* configuration,
                           std::string& error_message) const noexcept
-{
-  return std::shared_ptr<opentracing::Tracer>{};
+try {
+  YAML::Node yaml;
+  try {
+    yaml = YAML::Load(configuration);
+  } catch(const YAML::ParserException& e) {
+    error_message = e.what();
+    return opentracing::make_unexpected(opentracing::configuration_parse_error);
+  }
+
+  const auto serviceNameNode = yaml["service_name"];
+  if (!serviceNameNode) {
+    error_message = "`service_name` not provided";
+    return opentracing::make_unexpected(
+        opentracing::invalid_configuration_error);
+  }
+  if (!serviceNameNode.IsScalar()) {
+    error_message = "`service_name` must be a string";
+    return opentracing::make_unexpected(
+        opentracing::invalid_configuration_error);
+  }
+  std::string serviceName = serviceNameNode.Scalar();
+
+  const auto tracerConfig = jaegertracing::Config::parse(yaml);
+  return jaegertracing::Tracer::make(serviceName, tracerConfig);
+} catch (const std::bad_alloc&) {
+    return opentracing::make_unexpected(
+        std::make_error_code(std::errc::not_enough_memory));
+} catch (const std::exception& e) {
+    error_message = e.what();
+    return opentracing::make_unexpected(
+        opentracing::invalid_configuration_error);
 }
 }  // namespace jaegertracing
