d7d6a28c513bb5f0947b5c44e9865dad40961759
Ryan Burn <ryan.burn@gmail.com>
2018-02-23 15:11:02 -0500

    Remove unused function.

--- src/jaegertracing/DynamicLoad.cpp
+++ src/jaegertracing/DynamicLoad.cpp
@@ -8,25 +8,6 @@
 #include "jaegertracing/Tracer.h"
 #include "TracerFactory.h"
 
-int make_libjaegertracing_tracer(const char* config,
-                                 void* tracerSharedPtrPtr,
-                                 void* errorMsgStringPtr)
-{
-    auto& tracer = *static_cast<std::shared_ptr<opentracing::Tracer>*>(
-        tracerSharedPtrPtr);
-    auto& errorMsg = *static_cast<std::string*>(errorMsgStringPtr);
-    try {
-        const auto yaml = YAML::Load(config);
-        const auto serviceName = yaml["service_name"].as<std::string>();
-        const auto tracerConfig = jaegertracing::Config::parse(yaml);
-        tracer = jaegertracing::Tracer::make(serviceName, tracerConfig);
-        return 0;
-    } catch (const std::exception& e) {
-        errorMsg.assign(e.what());
-        return -1;
-    }
-}
-
 int 
 opentracing_make_tracer_factory(const char* opentracingVersion,
                                 const void** errorCategory,
